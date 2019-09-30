02914fd6649ce2894af1c5eec94b29ecbd469db7
Melissa Christman <melchrstmn@gmail.com>
2019-09-30 16:23:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(array)
   new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? { |e| e.size > 4  }
 end
 
 def find_the_cheese# code an argument here
