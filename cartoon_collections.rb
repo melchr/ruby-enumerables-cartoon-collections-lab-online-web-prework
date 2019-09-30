39a6e23d76e990804d6a193a4865b8efe4f31ab1
Melissa Christman <melchrstmn@gmail.com>
2019-09-30 16:23:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,8 @@ def long_planeteer_calls(array)
   array.any? { |e| e.size > 4  }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredients.each_with_index { |value, index| return ingredients[index] if cheese_types.include?(value)}
+  nil
 end
